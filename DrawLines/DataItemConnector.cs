945848a780e524a09afffdc6744e2fe7e3638ae9
Arda Aktari <a.aktari@inno-focus.com>
2022-04-06 10:53:33 +0200

    InitialCommit

---
+++ DrawLines/DataItemConnector.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DrawLines
+{
+    public class DataItemConnector : PropertyChangedBase
+    {
+        public object Start { get; set; }
+        public object End { get; set; }
+
+        private Point _startPoint;
+        public Point StartPoint
+        {
+            get { return _startPoint; }
+            set
+            {
+                _startPoint = value;
+                OnPropertyChanged("StartPoint");
+            }
+        }
+
+        private Point _endPoint;
+        public Point EndPoint
+        {
+            get { return _endPoint; }
+            set
+            {
+                _endPoint = value;
+                OnPropertyChanged("EndPoint");
+            }
+        }
+    }
+}
